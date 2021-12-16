fa0cac584c6449c937fa30f59bc183606f361cd4
ndizdar <81266615+ndizdar@users.noreply.github.com>
2021-12-16 14:41:44 -0500

    Add files via upload

---
+++ analysis/DMDensity.py
@@ -0,0 +1,83 @@
+from astropy.io import fits
+from astropy.utils.data import get_pkg_data_filename
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import scipy as sp
+
+
+def initializePosition():
+    hdulist = fits.open('474final\Data\DarkMatter_position_final.fits')
+    hdulist.info()
+
+    Positionarray = np.array(hdulist[0].data)
+    hdulist.close()
+    return Positionarray
+
+def gausskernel(r,h):
+    return (h * math.sqrt(math.pi))**-3 * math.exp(-r * r / (h * h))
+
+def kernel(r, h):
+    q = r / h
+    if (q < 0.5):
+        return 8.0 /math.pi/ (h*h*h) * (1.0 - 6.0 * q * q + 6.0 * q**3)   
+    elif (q < 1.0):
+        return 16.0 / math.pi / (h*h*h) * (1.0 - q)**3
+    return 0.0
+
+def toykernel(r, h):
+    q = r / h
+    Cs = 1/(4*math.pi*(h*h*h))
+    if (q < 1):
+        return Cs * ((2 - q)**3 - (4*(1 - q)**3))   
+    elif (q < 2):
+        return Cs * ((2 - q)**3)
+    return 0.0
+
+
+def density(x0, y0, z0, h, M, positions):
+
+  rho = 0
+  N = len(positions[:,0])
+  m = M/N
+
+  for i in range(N):
+    x = positions[i,0]
+    y = positions[i,1]
+    r = math.sqrt((x0-x)**2+(y0-y)**2)
+    rho += m * gausskernel(r, h)
+  return rho
+
+m = 2/1000
+h = 0.25
+y = 0
+z = 0
+M = 1
+R = 0.75
+n = 1
+a = 1
+
+Positionarray = initializePosition()
+
+rlist = []
+dlist = []
+validlist = []
+jlist = []
+
+for i in np.arange(0,5,0.01):
+    dx = density(i,0,0, h, M, Positionarray)
+    dy = density(0,i,0, h, M, Positionarray)
+
+    valid = math.sqrt(2*math.pi)*(M/(2*math.pi*a**3))*(1+(i**2)/(a**2))**(-3/2)
+    if valid >= 0:
+        jlist.append(i)
+        validlist.append(valid)
+    rlist.append(i)
+    dlist.append((dx+dy)/2)
+
+plt.scatter(rlist, dlist, s=2, c= 'k')
+plt.plot(jlist,validlist, c = 'r')
+plt.title("Density Graph")
+plt.xlabel("R")
+plt.ylabel("Density")
+plt.savefig("DMDensity.png")
