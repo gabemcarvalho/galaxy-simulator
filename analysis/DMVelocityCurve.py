edb28422944a9c485d83e7ce3b4fd7f0f09ea756
ndizdar <81266615+ndizdar@users.noreply.github.com>
2021-12-20 22:13:21 -0500

    Add files via upload

--- analysis/DMVelocityCurve.py
+++ analysis/DMVelocityCurve.py
@@ -18,20 +18,35 @@ def initializeVelocity():
     vdulist.close()
     return Velocityarray  
 
-def averaging(array):
+def averaging(circv):
     xlist = []
     average = []
-    for i in np.arange(0,10,0.1):
+    for i in np.arange(0,40,0.2):
         ylist = []
-        for j in range(len(array[:,0])):
-            if array[j,0] <= i and array[j,0] >= i-0.1:
-                ylist.append(array[j,1])
+        for j in range(len(circv[:,0])):
+            if circv[j,0] <= i and circv[j,0] >= i-0.2:
+                ylist.append(circv[j,1])
         
         if len(ylist) > 0:
             average.append(sum(ylist)/len(ylist))
             xlist.append(i)
         ylist = []
     return xlist, average
+
+def rollingaverage(xlist,average):
+    rllavg = []
+    for i in range(len(average)):
+        if i < 5:
+            rllavg.append(average[i])
+        elif i < len(average) - 5:
+            rollavg = (average[i-5]+ average[i-4] + average[i-3] + average[i-2] + average[i-1] + 
+            average[i] + average[i+1] + average[i+2] + average[i+3]+ average[i+4]+ average[i+5])/11
+            rllavg.append(rollavg)
+        else:
+            rllavg.append(average[i])
+
+    return xlist, rllavg
+
 
 Velocityarray = initializeVelocity()
 Positionarray = initializePosition()
@@ -45,6 +60,7 @@ rlist = []
 circvlist = []
 dispvlist = []
 for i in range(len(Positionarray[:,0])):
+
     x = Positionarray[i,0]
     y = Positionarray[i,1]
     z = Positionarray[i,2]
@@ -56,7 +72,7 @@ for i in range(len(Positionarray[:,0])):
     dispersionvels = xv**2+yv**2+zv**2
     dispvlist.append(dispersionvels)
     r = math.sqrt(x**2+y**2+z**2)
-    circv = (xv*y - x*yv)/(x**2+y**2)
+    circv = (xv*y - x*yv)/(x**2+y**2)*r
     rlist.append(r)
     circvlist.append(abs(circv))
 
@@ -66,12 +82,13 @@ print("The total velocity dispersion is ", totdispersion)
 rotationarray = np.array(list(zip(rlist, circvlist)))
 sortedarray = rotationarray[rotationarray[:, 0].argsort()]
 avgr, avgy = averaging(sortedarray)
+avgr, avgy = rollingaverage(avgr, avgy)
 
-plt.scatter(sortedarray[:,0], sortedarray[:,1], s=2, c= 'k', alpha = 0.2)
+plt.scatter(sortedarray[:,0], sortedarray[:,1], s=2, c= 'k', alpha = 0.1)
 plt.plot(avgr, avgy, c= 'r')
 plt.title("Rotation Curve of Dark Matter")
 plt.xlabel("R in kpc")
-plt.xlim(0,10)
-plt.ylim(0,2)
+plt.xlim(0,30)
+plt.ylim(0,1.5)
 plt.ylabel("Circular Velocity (kpc/Gyr)")
 plt.savefig("DarkMatterRotationCurve.png")
